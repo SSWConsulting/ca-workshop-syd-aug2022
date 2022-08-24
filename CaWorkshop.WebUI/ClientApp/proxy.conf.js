28db6ebe9d84f2ce99577140ac5bcdbb8f291dbc
Jason Taylor <jasontaylor@ssw.com.au>
2022-08-24 10:03:12 +1000

    GettingStarted-01

---
+++ CaWorkshop.WebUI/ClientApp/proxy.conf.js
@@ -0,0 +1,25 @@
+const { env } = require('process');
+
+const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
+  env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:7240';
+
+const PROXY_CONFIG = [
+  {
+    context: [
+      "/_configuration",
+      "/.well-known",
+      "/Identity",
+      "/connect",
+      "/ApplyDatabaseMigrations",
+      "/_framework",
+      "/api"
+   ],
+    target: target,
+    secure: false,
+    headers: {
+      Connection: 'Keep-Alive'
+    }
+  }
+]
+
+module.exports = PROXY_CONFIG;
