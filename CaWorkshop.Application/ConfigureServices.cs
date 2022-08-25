8150659c5a0d60a54d3237eb45389dc5a2c6ad12
Jason Taylor <jasontaylor@ssw.com.au>
2022-08-25 10:22:55 +1000

    BeyondBasics04

--- CaWorkshop.Application/ConfigureServices.cs
+++ CaWorkshop.Application/ConfigureServices.cs
@@ -8,6 +8,7 @@ public static class ConfigureServices
     public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
         services.AddMediatR(Assembly.GetExecutingAssembly());
+        services.AddAutoMapper(Assembly.GetExecutingAssembly());
 
         return services;
     }
